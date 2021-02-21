b93730ce59006fe869c3be0afb15dc3e9e0944b6
Kurtuluş ERTÜRK <47227072+kurtuluserturk@users.noreply.github.com>
2021-02-21 16:50:55 +0300

    error page
    error page added

--- src/components/Default.js
+++ src/components/Default.js
@@ -2,8 +2,21 @@ import React from 'react'
 
 const Default = () => {
     return (
-        <div>
-            <h3>page not found</h3>
+        <div className="container">
+            <div className="row">
+                <div className="col-10 mx-auto text-center text-uppercase text-title pt-5">
+                    <h1 className="display-3">404</h1>
+                    <h1>error</h1>
+                    <h2>page not found</h2>
+                    <h3>
+                        the requested URL
+                        <span className="text-danger">
+                            {window.location.pathname}
+                        </span>{" "}
+                        was not found
+                    </h3>
+                </div>
+            </div>
         </div>
     )
 }
